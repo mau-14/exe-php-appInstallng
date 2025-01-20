04be9066d5ce3be2d8b7b5e52674ebeb225f1fbf
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-20 18:45:34 +0100

    bd connect

--- app/ejecutable.php
+++ app/ejecutable.php
@@ -1,10 +1,19 @@
 <?php 
 
+require_once './config/configDb.php';
 
 try {
 
+  $conexion = new mysqli(SERVIDOR,USUARIO,PASSWORD,BBDD);
+  if($conexion->connect_error){
+    throw new Exception('No se pudo conectar con la base de datos');
+  }
 
   
-  } catch (Throwable $th) {
-    //throw $th;
-  }
+
+
+
+} catch (Throwable $th) {
+  error_log($th->getMessage());
+  die('Se ha producido un error, revise los logs del servidor');
+}
